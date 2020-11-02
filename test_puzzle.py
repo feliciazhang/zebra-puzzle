7a7efa0c9b2be6d360134c832bc1991878e3b691
Felicia Zhang <fy.zhang2016@gmail.com>
2020-11-01 19:56:52 -0500

    translate min (#2)

--- test_puzzle.py
+++ test_puzzle.py
@@ -2,7 +2,7 @@
 
 import unittest
 import puzzle as P
-from pyeda.inter import And, OneHot
+from pyeda.inter import And, OneHot, Or
 
 class LogicTest(unittest.TestCase):
     root_group = ['1', '2', '3']
@@ -17,9 +17,8 @@ class LogicTest(unittest.TestCase):
         self.assertEqual(self.ex_puzzle.var_to_id("sleep_1"), (1, 1, 0))
         self.assertEqual(self.ex_puzzle.var_to_id("Dibii_2"), (2, 2, 1))
 
-    def test_id_to_var(self):
-        self.assertEqual(self.ex_puzzle.id_to_var([1, 1, 0]), "sleep_1")
-        self.assertEqual(self.ex_puzzle.id_to_var(self.ex_puzzle.var_to_id("Dibii_2")), "Dibii_2")
+    def test_fid_to_var(self):
+        self.assertEqual(self.ex_puzzle.fid_to_var('x[1,1,0]'), "sleep_1")
 
     def test_get_val_tuple(self):
         self.assertEqual(self.ex_puzzle.get_val_tuple("sleep"), (1, 1))
@@ -34,10 +33,18 @@ class LogicTest(unittest.TestCase):
         away2 = And(self.ex_puzzle.X[0,2,2], self.ex_puzzle.X[2,0,0])
         away1 = (OneHot(And(self.ex_puzzle.X[0,2,2], self.ex_puzzle.X[2,0,1]),
         And(self.ex_puzzle.X[0,2,1], self.ex_puzzle.X[2,0,0])))
+        away_neg = And(self.ex_puzzle.X[0,2,2], self.ex_puzzle.X[2,0,0])
 
         self.assertTrue(self.ex_puzzle.is_x_away("Batman", "Starbuck", 2).equivalent(away2))
         self.assertTrue(self.ex_puzzle.is_x_away("Batman", "Starbuck", 1).equivalent(away1))
+        self.assertTrue(self.ex_puzzle.is_x_away("Starbuck", "Batman", -2).equivalent(away_neg))
 
+    def test_translate_f(self):
+        f = (Or(And(self.ex_puzzle.X[0,2,2], self.ex_puzzle.X[2,0,1]),
+        And(self.ex_puzzle.X[0,2,1], self.ex_puzzle.X[2,0,0])))
+        f_str = 'Or(And(Starbuck_3, Batman_2), And(Starbuck_2, Batman_1))'
+
+        self.assertEqual(self.ex_puzzle.translate_f(f), f_str)
 
 
 if __name__ == "__main__":
